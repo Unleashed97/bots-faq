1cd38063c0dd145064af6295e3d80ecd76cb5ae3
Nichkov Roman <nichkovroman@gmail.com>
2022-12-14 18:11:35 +0300

    feat: menu toggle

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,30 @@
+export const toggleSidebarMenu = () => {
+    const captionList = document.querySelectorAll('.nav__item-caption')
 
+    if (captionList) {
+        captionList.forEach((caption) =>
+            caption.addEventListener('click', () => {
+                const captionParentItem = caption.closest('.nav__item')
+
+                captionParentItem.classList.toggle('active')
+
+                const captionBottomList =
+                    captionParentItem.querySelector('.nav__list')
+
+                // if (!captionBottomList.classList.contains('hidden')) {
+
+                // }
+
+                // captionBottomList.classList.toggle('hidden')
+            }),
+        )
+    }
+}
+
+// export const handleBurger = () => {
+//     const burgerBtn = document.querySelector('.burger')
+
+//     burgerBtn.addEventListener('click', () => {
+//         burgerBtn.classList.toggle('active')
+//     })
+// }
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 import * as customFunctions from './modules/functions.js'
-
+customFunctions.toggleSidebarMenu()
+// customFunctions.handleBurger()
 // vendors
